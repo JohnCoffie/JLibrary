d000db39686aad7efa1e5737b0ba0da506a3b23e
JohnCoffie <wanderer1224@sohu.com>
2014-04-25 10:43:20 +0800

    added annotations to load the class properly.

--- src/main/java/jlibrary/service/UserManagerImpl.java
+++ src/main/java/jlibrary/service/UserManagerImpl.java
@@ -3,11 +3,17 @@ package jlibrary.service;
 import jlibrary.dao.UserDao;
 import jlibrary.entity.User;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("userManager")
 public class UserManagerImpl implements UserManager{
 
+	@Autowired
 	private UserDao userDao;
 	
 	public User getUserById(int id) {
 		return userDao.findUserById(id);
 	}
+
 }
