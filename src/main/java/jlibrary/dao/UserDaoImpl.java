3528b72650671d8db93ec98c40b363f302e3f39a
JohnCoffie <wanderer1224@sohu.com>
2014-04-25 10:44:35 +0800

    added annotations

--- src/main/java/jlibrary/dao/UserDaoImpl.java
+++ src/main/java/jlibrary/dao/UserDaoImpl.java
@@ -4,17 +4,23 @@ import jlibrary.entity.User;
 
 import org.hibernate.Query;
 import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+@Repository("userDao")
 public class UserDaoImpl implements UserDao {
 
+	@Autowired
 	private SessionFactory sessionFactory;
 	
 	@Override
 	public User findUserById(int id) {
-		String hql = "from User where user_id = ?";
+		String hql = "from User u where u.userId = ?";
 		Query query = sessionFactory.getCurrentSession().createQuery(hql);
+		//Query query = sessionFactory.getCurrentSession().createSQLQuery("select * from users where user_id = ?");
 		query.setInteger(0, id);
-		return (User)query.uniqueResult();
+		User user = (User)query.uniqueResult();
+		return user;
 	}
 
 }
