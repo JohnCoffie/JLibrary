b5a4da51897e504ee3d49f56430c1ee547f0bd46
JohnCoffie <wanderer1224@sohu.com>
2014-04-25 10:53:19 +0800

    added annotation settings

--- src/main/java/jlibrary/controller/LoginController.java
+++ src/main/java/jlibrary/controller/LoginController.java
@@ -2,9 +2,11 @@ package jlibrary.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import jlibrary.dao.UserDao;
 import jlibrary.entity.User;
 import jlibrary.service.UserManager;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,7 +14,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user") 
 public class LoginController {
 
+	@Autowired
 	private UserManager userManager;
+	@Autowired
+	private UserDao userDao;
 	
 	@RequestMapping("/login")
 	public String login(String username, String password, HttpServletRequest request) {
@@ -21,11 +26,14 @@ public class LoginController {
 		if ( user == null ) {
 			System.out.println("No such user");
 			return "login";
+			
 		} else if ( !password.equals(user.getPassword()) ) {
 			System.out.println("No such user");
 			return "login";
+			
 		} else {
 			return "welcome";
 		}
 	}
+
 }
