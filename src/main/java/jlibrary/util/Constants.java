ee99bba39e4c7fd71a75372e109e22f267c6819b
JohnCoffie <wanderer1224@sohu.com>
2014-05-04 16:47:09 +0800

    added a setting for main page

--- src/main/java/jlibrary/util/Constants.java
+++ src/main/java/jlibrary/util/Constants.java
@@ -3,4 +3,7 @@ package jlibrary.util;
 public class Constants {
 
 	public static final String HOME_PAGE = "login";
+	public static final String MAIN_PAGE = "main";
+	
+	public static final String BOOK_LIST_PAGE = "book/bookList";
 }
