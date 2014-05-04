291c7ee3a3ddc3249a7cd6064d2d0ebff90c3a48
JohnCoffie <wanderer1224@sohu.com>
2014-05-04 16:00:08 +0800

    added a new class

---
+++ src/main/java/jlibrary/entity/BookCategory.java
@@ -0,0 +1,43 @@
+package jlibrary.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name="book_category")
+public class BookCategory implements Serializable{
+
+	@Id
+	@GeneratedValue(generator="system-id") 
+	@GenericGenerator(name="system-id",strategy="increment") 
+	@Column(name = "category_id", unique = true, nullable = false) 
+	private int bookCategoryId;
+	
+	@Column(name = "category_name", nullable = false) 
+	private String bookCategoryName;
+
+	public int getBookCategoryId() {
+		return bookCategoryId;
+	}
+
+	public void setBookCategoryId(int bookCategoryId) {
+		this.bookCategoryId = bookCategoryId;
+	}
+
+	public String getBookCategoryName() {
+		return bookCategoryName;
+	}
+
+	public void setBookCategoryName(String bookCategoryName) {
+		this.bookCategoryName = bookCategoryName;
+	}
+	
+	
+}
