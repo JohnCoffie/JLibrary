37dce2f0b24248333eb61996424038f41cb17c41
JohnCoffie <wanderer1224@sohu.com>
2014-05-16 17:21:43 +0800

    added in a few methods.

--- src/main/java/jlibrary/service/BookManager.java
+++ src/main/java/jlibrary/service/BookManager.java
@@ -3,8 +3,14 @@ package jlibrary.service;
 import java.util.List;
 
 import jlibrary.entity.Book;
+import jlibrary.entity.BookCategory;
 
 public interface BookManager {
 
 	public List<Book> findAllBooks();
+	public List<BookCategory> findAllBookCategories();
+	public void addBook(Book book);
+	public void updateBook(Book book);
+	public Book getBookById(int id);
+	public void removeBooks(List<Integer> ids);
 }
--- src/main/java/jlibrary/service/BookManagerImpl.java
+++ src/main/java/jlibrary/service/BookManagerImpl.java
@@ -4,9 +4,13 @@ import java.util.List;
 
 import jlibrary.dao.BookDao;
 import jlibrary.entity.Book;
+import jlibrary.entity.BookCategory;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service("bookManager")
 public class BookManagerImpl implements BookManager {
@@ -16,7 +20,33 @@ public class BookManagerImpl implements BookManager {
 	
 	@Override
 	public List<Book> findAllBooks() {
-		return bookDao.findAll();
+		return bookDao.findAllBooks();
+	}
+	
+	@Override
+	public List<BookCategory> findAllBookCategories(){
+		return bookDao.findAllBookCategories();
+	}
+	
+	@Override
+	public void addBook(Book book){
+		bookDao.addBook(book);
 	}
 
+	@Override
+	public void updateBook(Book book){
+		bookDao.updateBook(book);
+	}
+	
+	@Override
+	public Book getBookById(int id){
+		return bookDao.getBookById(id);
+	}
+	
+	@Override
+	public void removeBooks(List<Integer> ids){
+		for ( Integer id : ids ) {
+			bookDao.removeBook(Book.class, id);
+		}
+	}
 }
