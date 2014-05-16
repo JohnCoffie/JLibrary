11e762ef68e876bdb6884bc883e84889aa367102
JohnCoffie <wanderer1224@sohu.com>
2014-05-16 17:17:43 +0800

    added a few definitions

--- src/main/java/jlibrary/util/Constants.java
+++ src/main/java/jlibrary/util/Constants.java
@@ -6,4 +6,8 @@ public class Constants {
 	public static final String MAIN_PAGE = "main";
 	
 	public static final String BOOK_LIST_PAGE = "book/bookList";
+	public static final String BOOK_ADD_PAGE = "book/bookAdd";
+	public static final String BOOK_UPDATE_PAGE = "book/bookUpdate";
+	
+	public static final String BOOK_LIST_ACTION = "book/list";
 }
