cb917f975e84ce07b681336de0e12ac60a91d223
JohnCoffie <wanderer1224@sohu.com>
2014-05-16 17:29:41 +0800

    added a few methods in.

--- src/main/java/jlibrary/dao/BookDao.java
+++ src/main/java/jlibrary/dao/BookDao.java
@@ -3,8 +3,14 @@ package jlibrary.dao;
 import java.util.List;
 
 import jlibrary.entity.Book;
+import jlibrary.entity.BookCategory;
 
 public interface BookDao {
 
-	public List<Book> findAll();
+	public List<Book> findAllBooks();
+	public List<BookCategory> findAllBookCategories();
+	public void addBook(Book book);
+	public void updateBook(Book book);
+	public Book getBookById(int id);
+	public void removeBook(Class clazz, int id);
 }
--- src/main/java/jlibrary/dao/BookDaoImpl.java
+++ src/main/java/jlibrary/dao/BookDaoImpl.java
@@ -3,26 +3,61 @@ package jlibrary.dao;
 import java.util.List;
 
 import jlibrary.entity.Book;
+import jlibrary.entity.BookCategory;
 
 import org.apache.log4j.Logger;
 import org.hibernate.Query;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository("bookDao")
-public class BookDaoImpl implements BookDao {
+public class BookDaoImpl extends BaseDaoImpl implements BookDao {
 
 	private static final Logger log = Logger.getLogger(BookDaoImpl.class.getName());
 	
-	@Autowired
-	private SessionFactory sessionFactory;
+	@Override
+	public List<Book> findAllBooks() {
+		//String hql = "from Book";
+		//Query query = sessionFactory.getCurrentSession().createQuery(hql);
+		
+		log.debug("finding all Book instances");
+		return getAllObject(Book.class);
+	}
 	
 	@Override
-	public List<Book> findAll() {
-		String hql = "from Book";
-		Query query = sessionFactory.getCurrentSession().createQuery(hql);
-		return query.list();
+	public List<BookCategory> findAllBookCategories(){
+		log.debug("finding all BookCategory instances");
+
+		return getAllObject(BookCategory.class);
+	}
+	
+	@Override
+	public void addBook(Book book){	
+		//Session session = sessionFactory.getCurrentSession();
+		//session.saveOrUpdate(book);
+		
+		saveObject(book);
+		log.debug("book save successful");
+	}
+	
+	@Override
+	public void updateBook(Book book){
+		saveObject(book);
+		log.debug("book update successful");
+	}
+	
+	@Override
+	public Book getBookById(int id){
+		Book book = (Book)getObject(Book.class, id);
+		return book;
+	}
+	
+	@Override
+	public void removeBook(Class clazz, int id){
+		removeObject(clazz, id);
+		log.debug("book with id: "+id+" delete successful");
 	}
 
 }
